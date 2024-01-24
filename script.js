34604476b4affbfdd0d182c11e367d60f7f38afc
冯不游 <71683364+mefengl@users.noreply.github.com>
2024-01-24 19:34:58 +0800

    refactor: comment out unused search links
    The unused search links have been commented out to prevent unnecessary redirections.

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
 
     window.open(`https://dribbble.com/search/${encodedKeyword}`);
     window.open(`https://www.figma.com/community/search?resource_type=mixed&sort_by=relevancy&query=${encodedKeyword}&editor_type=all&price=all&creators=all`);
-    window.open(`https://artvee.com/main/?s=${encodedKeyword}`);
-    window.open(`https://same.energy/search?q=${encodedKeyword}`);
-    window.open(`https://archives.design/search/${encodedKeyword}`);
+    // window.open(`https://artvee.com/main/?s=${encodedKeyword}`);
+    // window.open(`https://same.energy/search?q=${encodedKeyword}`);
+    // window.open(`https://archives.design/search/${encodedKeyword}`);
 });
-
